2e2b083c23b5fc2ba5ff5adde89d988661a48999
João Vitor Lopes <joaovitorlps07@hotmail.com>
2025-12-25 16:14:14 -0300

    images resolved

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -24,7 +24,7 @@ export default function Body() {
             </div>
             <div className="hero-image">
               <img
-                src="hero-illustration.jpg"
+                src="/img/hero-illustration.jpg"
                 alt="Freelancers trabalhando"
               />
               <div className="floating-card card-1">
